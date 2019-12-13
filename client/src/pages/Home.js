60d016d0a9cf464ccc42270c21121e891cb5216f
allison <allisonchen1130@gmail.com>
2019-12-13 10:13:00 -0500

    Adding auth token to apollo context, creating and addings posts to cache

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,56 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
-import gql from "graphql-tag";
 import { Grid } from "semantic-ui-react";
+
+import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
 function Home() {
-  let posts = "";
+  const { user } = useContext(AuthContext);
+
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
 
+  let posts = "";
   if (data) {
     posts = { data: data.getPosts };
   }
+
   return (
     <Grid columns={3}>
       <Grid.Row className="page-title">
         <h1>Recent Posts</h1>
       </Grid.Row>
-      {loading ? (
-        <h1>Loading posts..</h1>
-      ) : (
-        posts.data &&
-        posts.data.map(post => (
-          <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-            <PostCard post={post} />
+      <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
           </Grid.Column>
-        ))
-      )}
-      <Grid.Row></Grid.Row>
+        )}
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          posts.data &&
+          posts.data.map(post => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
     </Grid>
   );
 }
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      userName
-      likeCount
-      likes {
-        userName
-      }
-      commentCount
-      comments {
-        id
-        userName
-        createdAt
-        body
-      }
-    }
-  }
-`;
 
 export default Home;
